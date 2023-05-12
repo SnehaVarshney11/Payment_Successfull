c8101b2668bcba371c442b6e5247519bac7d11de
SnehaVarshney11 <sneha.varshney_cs19@gla.ac.in>
2023-05-12 16:53:59 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary_btn_dark: "#11ABCD",
+        secondary: "#69239B",
+        tertiary: "#Fa1e82",
+        primary_dark: "#28232C",
+        secondary_dark:"#A1A1A1",
+        dark_button:" #342F38",
+        black: "#121212",
+        medium_gray: "#585858",
+        light_gray: "#f1f1f1",
+        error: "#e73318",
+        success: "#3dc518",
+        inprogress: "#f3880A",
+        footer: "#F1F1F1",
+        primary: "#E5397B",
+        "primary-text": "#121212",
+        green_card: '#3DC51B',
+        grey: '#777777',  
+        "vlight_grey": "#F5F5F5",
+      },
+    },
+  },
+  plugins: [],
+}
+
